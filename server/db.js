bf8f5e86f6539f5a1143f14aebb981777fb4a836
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-05-01 02:54:12 +0300

    added customers table to database

--- server/db.js
+++ server/db.js
@@ -9,33 +9,67 @@ const knex = require('knex')({
 })
 
 knex.schema
-.hasTable('users')
-.then((exists) => {
-    if (!exists) {
-    return knex.schema.createTable('users', (table)  => {
-        table.increments('id').primary()
-        table.string('username')
-        table.string('password')
-        table.string('area')
+    .hasTable('users')
+    .then((exists) => {
+        if (!exists) {
+        return knex.schema.createTable('users', (table)  => {
+            table.increments('id').primary()
+            table.string('username')
+            table.string('password')
+            table.string('area')
+        })
+        .then(() => {
+            console.log('Table \'users\' wurde erstellt')
+        })
+        .catch((error) => {
+            console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+        })
+        }
     })
     .then(() => {
-        console.log('Table \'users\' wurde erstellt')
+        console.log('users table done')
     })
     .catch((error) => {
-        console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
     })
-    }
-})
-.then(() => {
-    console.log('done')
-})
-.catch((error) => {
-    console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
-})
+
+knex.schema
+    .hasTable('customers')
+    .then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('customers', (table)  => {
+                table.increments('id').primary()
+                table.string('name')
+                table.integer('age')
+                table.string('address')
+                table.string('phone')
+                table.date('dateOfSubscription')
+                table.date('dateOfSubscriptionEnd')
+                table.integer('paidAmount')
+            })
+            .then(() => {
+                console.log('Table \'customers\' wurde erstellt')
+            })
+            .catch((error) => {
+                console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+            })
+        }
+    })
+    .then(() => {
+        console.log('customers table done')
+    })
+    .catch((error) => {
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
+    })
+
 
 //Zum Debuggen
 knex.select('*').from('users')
-  .then(data => console.log('data:', data))
+  .then(data => console.log('users data:', data))
   .catch(err => console.log(err))
-  
+
+knex.select('*').from('customers')
+    .then(data => console.log('customers data:', data))
+    .catch(err => console.log(err))
+
 module.exports = knex
