9d9fd6213d47bef98302eaf6a98a33dad9dfe567
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-04-30 21:49:09 +0300

    implemented a Session Storage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './css/App.css';
-import React, { useState } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import ManagementDashboard from './pages/ManagementDashboard/ManagementDashboard';
 import LoginPage from './pages/LoginPage/LoginPage';
-
+import useToken from './useToken';
 
 function App() {
-const [token, setToken] = useState();
+
+const { token, setToken } = useToken();
 
 if(!token) {
-  return <LoginPage setToken={setToken} />
+  return (
+  <div className='wrapper'>
+    <h1>Meal-Sys</h1> 
+    <LoginPage setToken={setToken} />
+  </div>
+  )
 }
 
   return (
