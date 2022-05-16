7da844debb028900d2677a864cde6182543325c7
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-05-16 22:29:15 +0300

    added customers router and controller

---
+++ server/controllers/customers-controller.js
@@ -0,0 +1,69 @@
+const knex = require('./../db')
+
+exports.customersAll = async (req, res) => {
+    knex
+        .select('*')
+        .from('customers')
+        .then(customerData => {
+            res.json(customerData)
+        })
+        .catch(err => {
+            res.json({ message: `Ein Fehler ist aufgetaucht: ${err}` })
+        })
+    }
+
+exports.customersFind = async (req, res) => {
+    knex
+        .select('*')
+        .from('customers')
+        .where('id', req.body.id)
+        .then(customerData => {
+            res.json(customerData)
+        })
+        .catch(err => {
+            res.json({ message: `Ein Fehler ist aufgetaucht: ${err}` })
+        })
+    }
+
+exports.customersCreate = async (req, res) => {
+    knex('customers')
+        .insert({
+            'name': req.body.name,
+            'age': req.body.age,
+            'address': req.body.address,
+            'phone': req.body.phone,
+            'dateOfSubscription': req.body.dateOfSubscription,
+            'dateOfSubscriptionEnd': req.body.dateOfSubscriptionEnd,
+            'paidAmount': req.body.paidAmount
+        })
+        .then(() => {
+            res.json({ message: 'Kunde wurde erfolgreich angelegt' })
+        })
+        .catch(err => {
+            res.json({ message: `Ein Fehler ist aufgetaucht: ${err}` })
+        })
+    }
+
+exports.customersDelete = async (req, res) => {
+    knex('customers')
+        .where('id', req.body.id)
+        .del()
+        .then(() => {
+            res.json({ message: 'Kunde wurde erfolgreich geloescht' })
+        })
+        .catch(err => {
+            res.json({ message: `Ein Fehler ist aufgetaucht: ${err}` })
+        })
+    }
+
+exports.customersUpdate = async (req, res) => {
+    knex('customers')
+        .where('id', req.body.id)
+        .update(req.body)  
+        .then(() => {
+            res.json({ message: 'Kunde wurde erfolgreich aktualisiert' })
+        })
+        .catch(err => {
+            res.json({ message: `Ein Fehler ist aufgetaucht: ${err}` })
+        })
+}
---
+++ server/routes/customers-route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const customersRoutes = require('./../controllers/customers-controller')
+
+router.get('/all', customersRoutes.customersAll)
+
+router.get('/find', customersRoutes.customersFind)
+
+router.post('/create', customersRoutes.customersCreate)
+
+router.put('/delete', customersRoutes.customersDelete)
+
+router.put('/update', customersRoutes.customersUpdate)
