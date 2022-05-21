48cec622d79b627a93e9b1f597a1ebc01acc1523
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-05-21 04:36:32 +0300

    add ButtonBar to App

--- src/pages/ManagementDashboard/ManagementDashboard.jsx
+++ src/pages/ManagementDashboard/ManagementDashboard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { ButtonBar } from '../../components/ButtonBar';
 import '../../css/ManagementDashboard.css';
 
 function logout() {
   sessionStorage.clear();
   window.location.href = '/';
 }
-
 export default function ManagementDashboard({ token }) {
   return(
     <div>
       <h2>Management Dashboard</h2>
       <p>Welcome User {token}</p>
       <button className='logoutButton' type='button' onClick={logout}>Logout</button>
+      <ButtonBar renderCustomers={true} renderProducts={true}/>
     </div>
     
   );
