a17f28fbcde17ecb1192504b9f55557673ad2e19
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-05-23 16:42:12 +0300

    add all other tabs to Management Dashboard

--- src/components/ButtonBar.jsx
+++ src/components/ButtonBar.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import { Customers } from './Customers/customers';
 
-export function ButtonBar({ renderCustomers, renderMenus, renderOrders, renderUsers }) {
+export function ButtonBar({ renderCustomers, renderMenus, renderOrders, renderUsers, renderPersonals, renderIngredients }) {
     const [viewCustomers, setViewCustomers] = useState(false);
     const [viewMenus, setViewMenus] = useState(false);
     const [viewOrders, setViewOrders] = useState(false);
     const [viewUsers, setViewUsers] = useState(false);
+    const [viewPersonals, setViewPersonals] = useState(false);
+    const [viewIngredients, setViewIngredients] = useState(false);
 
 
     const handleViewCustomers = () => {
         setViewMenus(false);
         setViewOrders(false);
         setViewUsers(false);
+        setViewPersonals(false);
+        setViewIngredients(false);
         setViewCustomers(!viewCustomers);
     };
 
@@ -19,6 +23,8 @@ export function ButtonBar({ renderCustomers, renderMenus, renderOrders, renderUs
         setViewCustomers(false);
         setViewOrders(false);
         setViewUsers(false);
+        setViewPersonals(false);
+        setViewIngredients(false);
         setViewMenus(!viewMenus);
     };
 
@@ -26,6 +32,8 @@ export function ButtonBar({ renderCustomers, renderMenus, renderOrders, renderUs
         setViewCustomers(false);
         setViewMenus(false);
         setViewUsers(false);
+        setViewPersonals(false);
+        setViewIngredients(false);
         setViewOrders(!viewOrders);
     };
 
@@ -33,18 +41,43 @@ export function ButtonBar({ renderCustomers, renderMenus, renderOrders, renderUs
         setViewCustomers(false);
         setViewMenus(false);
         setViewOrders(false);
+        setViewPersonals(false);
+        setViewIngredients(false);
         setViewUsers(!viewUsers);
     };
+
+    const handleViewPersonals = () => {
+        setViewCustomers(false);
+        setViewMenus(false);
+        setViewOrders(false);
+        setViewUsers(false);
+        setViewIngredients(false);
+        setViewPersonals(!viewPersonals);
+    };
+
+    const handleViewIngredients = () => {
+        setViewCustomers(false);
+        setViewMenus(false);
+        setViewOrders(false);
+        setViewUsers(false);
+        setViewPersonals(false);
+        setViewIngredients(!viewIngredients);
+    };
+
     return (
         <div className='button-wrapper'>
+            {renderUsers && <button className='UsersButton' type='button' onClick={handleViewUsers}>Users</button>}
+            {renderPersonals && <button className='PersonalsButton' type='button' onClick={handleViewPersonals}>Personals</button>}
             {renderCustomers && <button className='customersButton' type='button' onClick={handleViewCustomers}>Customers</button>}
             {renderMenus && <button className='MenusButton' type='button' onClick={handleViewMenus}>Menus</button>}
             {renderOrders && <button className='OrdersButton' type='button' onClick={handleViewOrders}>Orders</button>}
-            {renderUsers && <button className='UsersButton' type='button' onClick={handleViewUsers}>Users</button>}
+            {renderIngredients && <button className='IngredientsButton' type='button' onClick={handleViewIngredients}>Ingredients</button>}
+                {viewUsers && <div>Users</div>}
+                {viewPersonals && <div>Personals</div>}
                 {viewCustomers && <Customers />}
                 {viewMenus && <div>Menus</div>}
                 {viewOrders && <div>Orders</div>}
-                {viewUsers && <div>Users</div>}
+                {viewIngredients && <div>Ingredients</div>}
         </div>
     );
 }
