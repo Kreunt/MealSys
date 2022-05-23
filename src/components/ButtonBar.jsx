e7e269c38e1cac7a25c07a1c9eacbba20e1d598c
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-05-23 16:26:44 +0300

    add 2 more tabs to Management

--- src/components/ButtonBar.jsx
+++ src/components/ButtonBar.jsx
@@ -10,21 +10,41 @@ export function ButtonBar({ renderCustomers, renderMenus, renderOrders, renderUs
 
     const handleViewCustomers = () => {
         setViewMenus(false);
+        setViewOrders(false);
+        setViewUsers(false);
         setViewCustomers(!viewCustomers);
     };
 
     const handleViewMenus = () => {
         setViewCustomers(false);
+        setViewOrders(false);
+        setViewUsers(false);
         setViewMenus(!viewMenus);
     };
 
+    const handleViewOrders = () => {
+        setViewCustomers(false);
+        setViewMenus(false);
+        setViewUsers(false);
+        setViewOrders(!viewOrders);
+    };
 
+    const handleViewUsers = () => {
+        setViewCustomers(false);
+        setViewMenus(false);
+        setViewOrders(false);
+        setViewUsers(!viewUsers);
+    };
     return (
         <div className='button-wrapper'>
             {renderCustomers && <button className='customersButton' type='button' onClick={handleViewCustomers}>Customers</button>}
             {renderMenus && <button className='MenusButton' type='button' onClick={handleViewMenus}>Menus</button>}
+            {renderOrders && <button className='OrdersButton' type='button' onClick={handleViewOrders}>Orders</button>}
+            {renderUsers && <button className='UsersButton' type='button' onClick={handleViewUsers}>Users</button>}
                 {viewCustomers && <Customers />}
                 {viewMenus && <div>Menus</div>}
+                {viewOrders && <div>Orders</div>}
+                {viewUsers && <div>Users</div>}
         </div>
     );
 }
