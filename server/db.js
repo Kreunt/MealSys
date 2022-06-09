87cc4747b3512a905693e021a391bd688301d3f0
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-06-09 05:26:02 +0300

    create all db tables

--- server/db.js
+++ server/db.js
@@ -62,6 +62,135 @@ knex.schema
         console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
     })
 
+knex.schema
+    .hasTable('personal')
+    .then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('personal', (table)  => {
+                table.increments('id').primary()
+                table.string('name')
+                table.integer('age')
+                table.integer('workingHours')
+                table.string('freeDays')
+                table.string('area')
+                table.string('phone')
+            })
+            .then(() => {
+                console.log('Table \'personal\' wurde erstellt')
+            })
+            .catch((error) => {
+                console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+            })
+        }
+    })
+    .then(() => {
+        console.log('personal table done')
+    })
+    .catch((error) => {
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
+    })
+
+knex.schema
+    .hasTable('orders')
+    .then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('orders', (table)  => {
+                table.increments('id').primary()
+                table.string('customerId')
+                table.date('date')
+                table.integer('price')
+                table.string('menuId')
+            })
+            .then(() => {
+                console.log('Table \'orders\' wurde erstellt')
+            })
+            .catch((error) => {
+                console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+            })
+        }
+    })
+    .then(() => {
+        console.log('orders table done')
+    })
+    .catch((error) => {
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
+    })
+
+knex.schema
+    .hasTable('menus')
+    .then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('menus', (table)  => {
+                table.increments('id').primary()
+                table.string('name')
+                table.integer('price')
+                table.string('calories')
+            })
+            .then(() => {
+                console.log('Table \'menus\' wurde erstellt')
+            })
+            .catch((error) => {
+                console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+            })
+        }
+    })
+    .then(() => {
+        console.log('menus table done')
+    })
+    .catch((error) => {
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
+    })
+
+knex.schema
+    .hasTable('income_expense')
+    .then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('income_expense', (table)  => {
+                table.increments('id').primary()
+                table.integer('dailyIncome')
+                table.integer('dailyExpenses')
+                table.date('date')
+            })
+            .then(() => {
+                console.log('Table \'income_expense\' wurde erstellt')
+            })
+            .catch((error) => {
+                console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+            })
+        }
+    })
+    .then(() => {
+        console.log('income_expense table done')
+    })
+    .catch((error) => {
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
+    })
+
+knex.schema
+    .hasTable('ingredients')
+    .then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('ingredients', (table)  => {
+                table.increments('id').primary()
+                table.string('name')
+                table.integer('stock')
+                table.string('unit')
+            })
+            .then(() => {
+                console.log('Table \'ingredients\' wurde erstellt')
+            })
+            .catch((error) => {
+                console.error(`Ein Fehler ist aufgetaucht: ${error}`)
+            })
+        }
+    })
+    .then(() => {
+        console.log('ingredients table done')
+    })
+    .catch((error) => {
+        console.error(`Es gab einen Fehler waehrend des Erstellens der Datenbank: ${error}`)
+    })
+
 
 //Zum Debuggen
 knex.select('*').from('users')
@@ -70,6 +199,26 @@ knex.select('*').from('users')
 
 knex.select('*').from('customers')
     .then(data => console.log('customers data:', data))
+    .catch(err => console.log(err))
+    
+knex.select('*').from('personal')
+    .then(data => console.log('personal data:', data))
+    .catch(err => console.log(err))
+    
+knex.select('*').from('orders')
+    .then(data => console.log('orders data:', data))
+    .catch(err => console.log(err))
+
+knex.select('*').from('menus')
+    .then(data => console.log('menus data:', data))
+    .catch(err => console.log(err))
+
+knex.select('*').from('income_expense')
+    .then(data => console.log('income_expense data:', data))
+    .catch(err => console.log(err))
+
+knex.select('*').from('ingredients')
+    .then(data => console.log('ingredients data:', data))
     .catch(err => console.log(err))
 
 module.exports = knex
