120c0494f6a61b98d984218bf7aec323478d57d7
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-06-14 00:40:43 +0300

    add orders api endpoint

---
+++ server/routes/orders-route.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const ordersRoutes = require('./../controllers/orders-controller')
+
+router.get('/all', ordersRoutes.ordersAll)
+
+router.get('/find', ordersRoutes.ordersFind)
+
+router.post('/create', ordersRoutes.ordersCreate)
+
+router.put('/update', ordersRoutes.ordersUpdate)
+
+router.put('/delete', ordersRoutes.ordersDelete)
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const usersRouter = require('./routes/users-route.js')
 const usersTokenRouter = require('./routes/users-route-usertoken.js')
 const customersRouter = require('./routes/customers-route.js')
 const personalsRouter = require('./routes/personal-route.js')
+const ordersRouter = require('./routes/orders-route.js')
 
 const PORT = process.env.PORT || 4001
 
@@ -25,13 +26,14 @@ app.use('/api/login', usersTokenRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/customers', customersRouter)
 app.use('/api/personal', personalsRouter)
+app.use('/api/orders', ordersRouter)
 
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
     console.error(err.stack)
     res.status(500).send('Snap! Etwas ist gebrochen.')
 })
 
-app.use(function (req, res, next) {
+app.use(function (req, res) {
     console.log("this is 404", req.body)
     res.status(404).send('Sorry, wir konnten es nicht finden!')
 })
